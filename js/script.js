c65e73740a37022de04a06170a743fa34c5a4e4e
Azaliya <azaliya-frbi@rambler.ru>
2022-01-12 00:30:00 +0300

    less30 Измение веб-страницы

--- js/script.js
+++ js/script.js
@@ -12,15 +12,47 @@
 
 5) Добавить нумерацию выведенных фильмов */
 
-'use strict';
+"use strict";
 
 const movieDB = {
-    movies: [
-        "Логан",
-        "Лига справедливости",
-        "Ла-ла лэнд",
-        "Одержимость",
-        "Скотт Пилигрим против..."
-    ]
+  movies: [
+    "Логан",
+    "Лига справедливости",
+    "Ла-ла лэнд",
+    "Одержимость",
+    "Скотт Пилигрим против...",
+  ],
 };
 
+// const img = document.querySelectorAll(".promo__adv");
+const img = document.querySelectorAll(".promo__adv img");
+const adv_tit = document.querySelectorAll(".promo__adv-title ");
+//   imgs = item.querySelectorAll(".img");
+adv_tit[0].remove();
+img.forEach((item) => {
+  item.remove();
+});
+// обычная функция
+// img.forEach(function(item){
+//   item.remove();
+// });
+const poster = document.querySelector(".promo__bg"),
+  genre = poster.querySelector(".promo__genre"),
+  movieList = document.querySelector(".promo__interactive-list");
+genre.textContent = "Трагедия";
+poster.style.backgroundImage = 'url("img/bg.jpg")';
+// 3 task
+// movieList.forEach((item) => {
+//   item.remove();
+// });
+
+movieList.innerHTML = "";
+
+movieDB.movies.sort();
+movieDB.movies.forEach((film, i) => {
+  movieList.innerHTML += `
+    <li class="promo__interactive-item">${i + 1} ${film}
+        <div class="delete"></div>
+    </li>    
+    `;
+});
